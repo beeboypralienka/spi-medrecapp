729eaae63467f058fc4c6529a0d0a2fe419559bb
beeboypralienka <beeboy_pralienka@rocketmail.com>
2016-06-13 16:05:03 +0700

    add the jautomate script in junit

--- test/spi/medrecapp/Gui/FrmMainTest.java
+++ test/spi/medrecapp/Gui/FrmMainTest.java
@@ -5,6 +5,8 @@
  */
 package spi.medrecapp.Gui;
 
+import jautomate.ScriptLoggerTest;
+import jautomate.ScriptRunner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -13,19 +15,22 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.sikuli.script.Screen;
 
 /**
  *
  * @author fachrulpbm
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FrmMainTest {
     
     private static FrmMain fm;
     
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void setUpClass() throws Exception {               
         try {
             UIManager.setLookAndFeel(new NimbusLookAndFeel());
         } catch (UnsupportedLookAndFeelException ex) {
@@ -37,54 +42,42 @@ public class FrmMainTest {
     }
     
     @Test
-    public void scenario_01()throws Exception{
-        Screen screen = new Screen();
-        
-        /* Click master menu */
-        screen.click("/images/07master.png",0);
-        
-        /* Click submenu specialist data */
-        screen.click("/images/08specialistData.png",0);
-        
-        /* Click and type the specialist ID */
-        screen.type("/images/09typeSpecialistID.png", "SP-001",0);
-        
-        /* Type TAB + type "Internal Disease" on specialist Name + type TAB + type "50000" on price */
-        screen.type("\tInternal Disease\t50000");
-        
-        /* Click the insert button */
-        screen.click("/images/10insertButton.png",0);
-        
-        /* Check the information icon */
-        screen.exists("/images/11information.png",0);
-        
-        /* Click the OK button */
-        screen.click("/images/12okButton.png",0);
-        
-        /* Check if the data on table exist */
-        screen.exists("/images/13tabel.png",0);
-        
-        /* Click the data on table */
-        screen.click("/images/14tabelClick.png",0);
-        
-        /* Check if the field automated filled */
-        screen.exists("/images/15fieldAuto.png",0);
-        
-        /* Click the delete button */
-        screen.click("/images/16deleteButton.png",0);
-        
+    public void a_scenario_01()throws Exception{
+        //System.out.println("*** [This is SikuliX test] ***");
+        Screen screen = new Screen();                                
+        screen.click("/images/07master.png",0);                
+        screen.click("/images/08specialistData.png",0);                
+        screen.type("/images/09typeSpecialistID.png", "SP-001",0);                
+        screen.type("\tInternal Disease\t50000");                
+        screen.click("/images/10insertButton.png",0);                
+        screen.exists("/images/11information.png",0);                
+        screen.click("/images/12okButton.png",0);                
+        screen.exists("/images/13tabel.png",0);                
+        screen.click("/images/14tabelClick.png",0);                
+        screen.exists("/images/15fieldAuto.png",0);                
+        screen.click("/images/16deleteButton.png",0);        
         screen.exists("/images/17question.png",0);
         screen.click("/images/18yesButton.png",0);
         screen.exists("/images/19information.png",0);
         screen.click("/images/20okButton.png",0);
         screen.exists("/images/21tableEmpty.png",0);
         screen.click("/images/22closeInternal.png",0);
-        
+        screen.click("/medrecapp.sikuli/file.png",0);
+        screen.click("/medrecapp.sikuli/exit.png",0);        
+    }
+    
+    @Test
+    public void b_scenario_2() {   
+        //System.out.println("*** [This is JAutomate test] ***");
+        ScriptRunner scriptRunner = new ScriptRunner(new ScriptLoggerTest());                
+        scriptRunner.setParameter("medrecapp.jautomate", "medrecapp.jautomate");
+        scriptRunner.runScript("medrecapp.jautomate/medrecapp-jautomate.txt");
     }
     
     @AfterClass
-    public static void tearDownClass() {
-        fm.setVisible(false);
+    public static void tearDownClass() {        
+        //System.out.println("---------------------------- End of SikuliX test ----------------------------\n\n");
+        //System.exit(0);
     }
     
 }
